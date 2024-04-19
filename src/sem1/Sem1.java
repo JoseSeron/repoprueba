06f1188d275de254df29406641f0d261189796cf
Jose R Seron <joserobertoseron@gmail.com>
2024-04-19 12:56:06 -0600

    commit 1 prueba

---
+++ src/sem1/Sem1.java
@@ -0,0 +1,17 @@
+
+package sem1;
+
+public class Sem1 {
+
+   
+    public static void main(String[] args) {
+        
+    
+    int [] notas = {100,60,70,80};
+        for (int contador = 0; contador < notas.length; contador++) {
+            System.out.printf("%d -> %3d\n",contador, notas[contador]);
+           
+        }
+    }
+    
+}
